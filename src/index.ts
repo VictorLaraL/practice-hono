4c49f260ce855dc9fd30f706ee171dfd6f404870
Victor Lara <vicblack15@hotmail.com>
2024-10-17 08:29:18 -0600

    change json response in / route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { Hono } from 'hono'
 
+
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.json({
+    "Hello": "World"
+  })
 })
 
 export default app
