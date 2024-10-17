d41e636bd6914d365ea0083d489868c8563f121e
Victor Lara <vicblack15@hotmail.com>
2024-10-17 10:44:51 -0600

    drizzle and db configurations ready

---
+++ src/db/schema.ts
@@ -0,0 +1,19 @@
+import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
+
+
+export const assistant = sqliteTable('assistant', {
+  // Modelo de la tabla de asistentes
+  id: integer('id').primaryKey(),
+  full_name: text('full_name').notNull(),
+  comment: text('comment'),
+  is_ceremony: integer('is_ceremony', {mode: 'boolean'}).notNull(),
+  is_celebration: integer('is_celebration', {mode: 'boolean'}).notNull()
+});
+
+export const song = sqliteTable('song', {
+  // Modelo de la tabla de canciones
+  id: integer('id').primaryKey(),
+  guest_name: text('guest_name').notNull(),
+  song_name: text('song_name').notNull(),
+  url: text('url')
+});
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import { Hono } from 'hono'
 
+type Env = {
+  MY_VAR: string
+}
 
-const app = new Hono()
+// Declaramos la instancia principal y le pasamos las variables de entorno
+// que declaramos anteriormente en el archivo wrangler.toml
+const app = new Hono<{Bindings: Env}>()
 
 app.get('/', (c) => {
   return c.json({
-    "Hello": "World"
+    "Hello": "World",
+    "VAR": c.env.MY_VAR
   })
 })
 
